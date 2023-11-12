8ba991c4a21d6c47099ce455856b58a30f9194cb
furkankaratekin <furkankaratekin.2k1@gmail.com>
2023-11-12 23:43:37 +0300

    add route pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
-
-import './App.css'
-
+import { BrowserRouter, Routes , Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import SignUp from "./pages/SignUp"
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>  
-    <h1 className='text-red-400'>sa</h1>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
