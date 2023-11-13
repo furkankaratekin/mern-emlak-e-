8b8b3c47241691357f9df0136c14156d778cee62
furkankaratekin <furkankaratekin.2k1@gmail.com>
2023-11-13 22:28:23 +0300

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -17,11 +18,22 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
 app.use("/api/user", userRouter)
+app.use('/api/auth', authRouter);
+
+
+
+
+
+
+
+
 //  "mongodb+srv://karatekin:karatekin@mern-try.akcva5f.mongodb.net/mern-try?retryWrites=true&w=majority"
 //Sdünetten bağlandığın zaman hata veriyor ve bağlanmıyor ayrıca connect metodun yanındaki urı'yı
 //dışarıdan çekmem gerekli fakat dışarıdan çekmede her zaman bir hata alıyorum.Deneme
